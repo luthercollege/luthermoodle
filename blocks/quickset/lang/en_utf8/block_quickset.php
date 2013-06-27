ca9fd06f644b3c5ce97fb0e6b64a91e68ffe3288
Bob Puffer <puffro01@luther.edu>
2013-06-27 13:58:13 -0500

    remove block_quickset in preparation for making it a submodule

--- blocks/quickset/lang/en_utf8/block_quickset.php
+++
@@ -1,9 +0,0 @@
-<?PHP // $Id$
-      // block_social_activities.php - created with Moodle 1.7 beta + (2006101003)
-
-
-$string['blockname'] = 'Quicksets';
-$string['reordersections'] = 'Update section changes';
-$string['addnewsectionsafterselected'] = 'Add new course section after selected';
-$string['remove'] = 'Remove selected sections';
-?>
